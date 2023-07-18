99524adf86dde7a22fc94ada166dc21c3bcb7f8b
Samuelwsz <Samuelxwsz@gmail.com>
2023-07-18 13:27:47 -0300

    att

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Botao from "./components/Botao"
+import Cardapio from "./pages/Cardapio"
+
+function App() {
+  return (
+    <>
+      <Cardapio />
+      <Botao>a</Botao>
+    </>
+  )
+}
+
+export default App
