8a20bcaf7ccf8edac2d396e8aee1eecc1ec1b7bd
Samuelwsz <Samuelxwsz@gmail.com>
2023-07-19 19:12:12 -0300

    att

--- src/pages/Cardapio/Buscador/index.tsx
+++ src/pages/Cardapio/Buscador/index.tsx
@@ -14,6 +14,7 @@ export default function Buscador({ busca, setBusca }: BuscadorProps) {
           type="text"
           value={busca}
           onChange={(evento) => setBusca(evento.target.value)}
+          placeholder="Buscar"
         />
         <CgSearch size={20} color="#4C4D5E" />
       </div>
